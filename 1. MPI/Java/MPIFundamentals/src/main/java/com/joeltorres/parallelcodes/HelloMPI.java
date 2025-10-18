f01cd3f03bed3f8cce030a4b34085188ca597a31
Hyoujin <quartzbyte@proton.me>
2025-10-17 20:49:00 -0500

    Updates java code with package.

--- 1. MPI/Java/MPIFundamentals/src/main/java/com/joeltorres/parallelcodes/HelloMPI.java
+++ 1. MPI/Java/MPIFundamentals/src/main/java/com/joeltorres/parallelcodes/HelloMPI.java
@@ -1,14 +1,16 @@
+package com.joeltorres.parallelcodes;
+
 import mpi.*;
 
 public class HelloMPI {
     public static void main(String[] args) throws MPIException {
         MPI.Init(args);
-        
+
         int rank = MPI.COMM_WORLD.Rank();
         int size = MPI.COMM_WORLD.Size();
-        
+
         System.out.println("Hello from process " + rank + " of " + size);
-        
+
         MPI.Finalize();
     }
 }
